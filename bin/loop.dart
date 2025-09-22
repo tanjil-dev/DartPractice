742bda1646dc9a130b281fa51d482dcc4168e488
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2025-09-22 07:52:52 +0600

    loop

--- bin/loop.dart
+++ bin/loop.dart
@@ -9,4 +9,15 @@ void main(){
     }
     print(i);
   }
+  for(int i =1; i<=10; i=i+2){
+    print(i); //post or pre increment doesn't change for loop parameter until the current iteration oif loop body ends
+  }
+  int i = 1;
+  while(i<10){
+    print("While Loop pre increment: ${++i}"); //while loop pre increment value possible not like for i explained above
+  }
+  List animal = ["A", "B", "C"];
+  for(int i = 0; i < animal.length; i++){
+    print(animal[i]);
+  }
 }
